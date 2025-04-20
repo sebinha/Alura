221859580abe82194c1ec6e09fcf8cd54864793e
Victor Fernandes <victorfcardoso@hotmail.com>
2025-04-19 21:23:46 -0300

    first commit

---
+++ Next/code-connect/src/components/CardPost/index.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Avatar } from "../Avatar";
+
+export const CardPost = ({ post }) => {
+    return (
+      <article>
+        <header>
+          <figure>
+            <Image src={post.cover} width={438} height={133} alt={`Card do post de título: ${post.title}`}/>
+          </figure>
+        </header>
+        <section>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </section>
+        <footer>
+          <Avatar 
+                  imageSrc={post.author.avatar} 
+                  name={post.author.username} 
+              />
+        </footer>
+      </article>
+    );
+  }

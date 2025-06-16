bf68582ecc9661fb1ccab4c18a6e5af12d8c3135
Victor Fernandes <victorfcardoso@hotmail.com>
2025-06-16 00:00:49 -0300

    feat: adds learning angular data pipes

--- Angular/First_steps/anybank/src/app/banner/banner.ts
+++ Angular/First_steps/anybank/src/app/banner/banner.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { BoasVindas } from './boas-vindas/boas-vindas';
+import { Conta } from './conta/conta';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [BoasVindas, Conta],
   templateUrl: './banner.html',
-  styleUrl: './banner.scss'
+  styleUrl: './banner.scss',
 })
 export class Banner {
-
+  data = new Date();
 }

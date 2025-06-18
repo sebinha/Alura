72e9fcaf176a36f591a4010a236839a769fc2902
Victor Fernandes <victorfcardoso@hotmail.com>
2025-06-18 01:00:15 -0300

    feat: adds learning ngModel and FormsModule

--- Angular/First_steps/anybank/src/app/app.ts
+++ Angular/First_steps/anybank/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Banner } from './banner/banner';
+import { FormNovaTransacao } from './form-nova-transacao/form-nova-transacao';
 
 @Component({
   selector: 'app-root',
-  imports: [Banner],
+  imports: [Banner, FormNovaTransacao],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ Angular/First_steps/anybank/src/app/form-nova-transacao/form-nova-transacao.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-nova-transacao',
+  imports: [],
+  templateUrl: './form-nova-transacao.html',
+  styleUrl: './form-nova-transacao.scss'
+})
+export class FormNovaTransacao {
+
+}

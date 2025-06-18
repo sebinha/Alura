4dba276a4bd9091b321464106a6dc55efd90240d
Victor Fernandes <victorfcardoso@hotmail.com>
2025-06-18 01:01:45 -0300

    feat: adds learning ngModel and FormsModule

--- Angular/First_steps/anybank/src/app/form-nova-transacao/form-nova-transacao.ts
+++ Angular/First_steps/anybank/src/app/form-nova-transacao/form-nova-transacao.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.html',
-  styleUrl: './form-nova-transacao.scss'
+  styleUrl: './form-nova-transacao.scss',
 })
 export class FormNovaTransacao {
+  valorTransacao = '';
+  tipoTransacao = '';
 
+  onSubmit() {
+    console.log('Transação enviada:', {
+      valor: this.valorTransacao,
+      tipo: this.tipoTransacao,
+    });
+    this.valorTransacao = '';
+    this.tipoTransacao = '';
+  }
 }
